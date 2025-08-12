3453a1451eb4b2d0d5939fe5df6460a43415ac3d
bebcon <hebebcon@naver.com>
2025-08-12 22:57:47 +0900

    Replace bcrypt with bcryptjs for compatibility

--- server.js
+++ server.js
@@ -2,7 +2,7 @@
 const express = require('express');
 const path =require('path');
 const fs = require('fs');
-const bcrypt = require('bcrypt');
+const bcrypt = require('bcryptjs');
 const nodemailer = require('nodemailer');
 const https = require('https');
 
